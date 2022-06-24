7ed1fe8317088eaa42ba5da684879dd34ad9f85d
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-06-24 12:21:51 +0100

    Added a car_right

--- frogger.py
+++ frogger.py
@@ -10,6 +10,7 @@ wn.tracer(0)
 
 wn.register_shape(image_dir + "frog.gif")
 wn.register_shape(image_dir + "car_left.gif")
+wn.register_shape(image_dir + "car_right.gif")
 
 pen = turtle.Turtle()
 pen.speed(0)
@@ -33,27 +34,35 @@ class Player(Sprite):
         Sprite.__init__(self, x, y, width, height, image)
 
     def up(self):
-        self.y += 45
+        self.y += 50
 
     def down(self):
-        self.y -= 45
+        self.y -= 50
     
     def right(self):
-        self.x += 45
+        self.x += 50
     
     def left(self):
-        self.x -= 45
+        self.x -= 50
 
 class Car(Sprite):
     def __init__(self, x, y, width, height, image, dx):
         Sprite.__init__(self, x, y, width, height, image)
         self.dx = dx
+
+    def update(self):
+        self.x += self.dx
+        if (self.x < -400):
+            self.x = 400
+        if (self.x > 400):
+            self.x = -400
     
 
 player = Player(0, -300, 40, 40, image_dir + "frog.gif")
 player.render(pen)
 
-car_left = Car(0, -255, 40, 121, image_dir + "car_left.gif", 45)
+car_left = Car(0, -250, 40, 121, image_dir + "car_left.gif", -5)
+car_right = Car(0, -200, 40, 121, image_dir + "car_right.gif", 5)
 
 wn.listen()
 wn.onkeypress(player.up, "Up")
@@ -64,6 +73,9 @@ wn.onkeypress(player.left, "Left")
 while True:
     player.render(pen)
     car_left.render(pen)
+    car_right.render(pen)
+    car_left.update()
+    car_right.update()
     wn.update()
     pen.clear()
 
