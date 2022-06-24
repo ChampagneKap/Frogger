861dd6c6b8dfd4fe2c9f145e8583636451c5ac01
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-06-24 11:57:12 +0100

    Adding image directory to image files

--- frogger.py
+++ frogger.py
@@ -1,11 +1,13 @@
 import turtle
 
+image_dir = "/Users/kacper/Downloads/VSCode Practice/hello/Frogger/"
+
 wn = turtle.Screen()
 wn.title("Frogger")
 wn.setup(600, 800)
 wn.bgcolor("black")
 
-wn.register_shape("frog.gif")
+wn.register_shape(image_dir + "frog.gif")
 
 pen = turtle.Turtle()
 pen.speed(0)
@@ -26,9 +28,9 @@ class Sprite():
 
 class Player(Sprite):
     def __init__(self, x, y, width, height, image):
-        Sprite.__init__(x, y, width, height, image)
+        Sprite.__init__(self, x, y, width, height, image)
 
-player = Player(0, -300, 40, 40, "frog.gif")
+player = Player(0, -300, 40, 40, image_dir + "frog.gif")
 player.render(pen)
 
 wn.mainloop()
