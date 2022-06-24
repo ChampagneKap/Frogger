d3767c4e464434e6992cbc406b397c1059fcf91d
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-06-24 11:42:35 +0100

    brew

--- frogger.py
+++ frogger.py
@@ -5,4 +5,12 @@ wn.title("Frogger")
 wn.setup(600, 800)
 wn.bgcolor("black")
 
+class Sprite():
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+
 wn.mainloop()
