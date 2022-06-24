fe8e126479f69a5adb61f5a802cd21a2d7dd8093
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-06-24 11:51:48 +0100

    Adding image & drawing player on game screen

--- frogger.py
+++ frogger.py
@@ -5,6 +5,12 @@ wn.title("Frogger")
 wn.setup(600, 800)
 wn.bgcolor("black")
 
+wn.register_shape("frog.gif")
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.hideturtle()
+
 class Sprite():
     def __init__(self, x, y, width, height, image):
         self.x = x
@@ -12,5 +18,17 @@ class Sprite():
         self.width = width
         self.height = height
         self.image = image
+
+    def render(self, pen):
+        pen.goto(self.x, self.y)
+        pen.shape(self.image)
+        pen.stamp()
+
+class Player(Sprite):
+    def __init__(self, x, y, width, height, image):
+        Sprite.__init__(x, y, width, height, image)
+
+player = Player(0, -300, 40, 40, "frog.gif")
+player.render(pen)
 
 wn.mainloop()
