d882620e6294ccc9bc08fda5a35b72edcf334362
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-06-24 12:07:26 +0100

    Added player movements

--- frogger.py
+++ frogger.py
@@ -6,6 +6,7 @@ wn = turtle.Screen()
 wn.title("Frogger")
 wn.setup(600, 800)
 wn.bgcolor("black")
+wn.tracer(0)
 
 wn.register_shape(image_dir + "frog.gif")
 
@@ -30,7 +31,30 @@ class Player(Sprite):
     def __init__(self, x, y, width, height, image):
         Sprite.__init__(self, x, y, width, height, image)
 
+    def up(self):
+        self.y += 45
+
+    def down(self):
+        self.y -= 45
+    
+    def right(self):
+        self.x += 45
+    
+    def left(self):
+        self.x -= 45
+
 player = Player(0, -300, 40, 40, image_dir + "frog.gif")
 player.render(pen)
+
+wn.listen()
+wn.onkeypress(player.up, "Up")
+wn.onkeypress(player.down, "Down")
+wn.onkeypress(player.right, "Right")
+wn.onkeypress(player.left, "Left")
+
+while True:
+    player.render(pen)
+    wn.update()
+    pen.clear()
 
 wn.mainloop()
